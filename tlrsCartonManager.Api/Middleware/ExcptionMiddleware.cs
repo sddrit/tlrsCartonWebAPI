2ef0418af882ebed4e8c21df48397b190ca8474e
sddrit <sddrit@hotmail.com>
2021-03-28 18:48:31 +0530

    UpdateStartUp

---
+++ tlrsCartonManager.Api/Middleware/ExcptionMiddleware.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Text.Json;
+using tlrsCartonManager.Api.Error;
+
+namespace tlrsCartonManager.Api.Middleware
+{
+    public class ExcptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExcptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+
+        public ExcptionMiddleware(RequestDelegate next, ILogger<ExcptionMiddleware> logger, IHostEnvironment env)
+        {
+            _next = next;
+            _logger = logger;
+            _env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment()
+
+
+
+                ? new APIException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+                : new APIException(context.Response.StatusCode, "Internal Server Error");
+
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+
+            }
+        }
+    }
+}
