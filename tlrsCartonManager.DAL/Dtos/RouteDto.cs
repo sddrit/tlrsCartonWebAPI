be44a0a11bea129dfb0922bf4509e074e7b42aa4
Administrator <Administrator@SDDRIT-DEVSRV01>
2021-04-02 18:45:44 +0530

    Route  Status Column to Active Metadata

--- tlrsCartonManager.DAL/Dtos/RouteDto.cs
+++ tlrsCartonManager.DAL/Dtos/RouteDto.cs
@@ -12,7 +12,7 @@ namespace tlrsCartonManager.DAL.Dtos
         [Key]       
         public int Id { get; set; }      
         public string Description { get; set; }       
-        public int? Status { get; set; }
+        public bool? Active { get; set; }
     }
 
 }
