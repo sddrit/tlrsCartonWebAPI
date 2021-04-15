bd6a77dfc4b823baa91db19b9414927bd5b7c07c
Administrator <Administrator@SDDRIT-DEVSRV01>
2021-04-15 20:17:21 +0530

    Pick List

---
+++ tlrsCartonManager.DAL/Utility/PickListStoredProcedure.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tlrsCartonManager.DAL.Utility
+{
+   
+    public static class PickListStoredProcedure
+	{
+        public static string StoredProcedureName = "pickListInsertUpdateDelete";
+        public static List<string> StoredProcedureParameters = new List<string>()
+        {
+            "@pickListNo",
+            "@statementType",
+            "@userId",
+            "@pickList"
+
+
+        };
+        public static List<string> StoredProcedureTypeNames = new List<string>()
+        {
+            "dbo.udtPickList",
+            
+        };
+
+        public static string Sql = "EXEC " + StoredProcedureName + " " + string.Join(",", StoredProcedureParameters);
+	}
+	
+}
