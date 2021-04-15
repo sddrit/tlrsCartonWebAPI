bf0bc9fd686aa14859e79d6af220cfdb5bcffa22
Administrator <Administrator@SDDRIT-DEVSRV01>
2021-04-15 20:18:17 +0530

    Pick List

---
+++ tlrsCartonManager.DAL/Dtos/Pick/PickListDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tlrsCartonManager.DAL.Dtos.Pick
+{
+    public class PickListDto
+    {
+        [Key]     
+        public long TrackingId { get; set; }       
+        public string PickListNo { get; set; }     
+        public int CartonNo { get; set; }       
+        public string Barcode { get; set; }       
+        public string LocationCode { get; set; }      
+        public string WareHouseCode { get; set; }      
+        public string LastSentDeviceId { get; set; }       
+        public int? AssignedUserId { get; set; }       
+        public string RequestNo { get; set; }
+        public int PickedUserId { get; set; }       
+        public bool IsPicked { get; set; }      
+        public long? PickDate { get; set; }     
+      
+    }
+    public class PickListSearchDto
+    {
+        public string PickListNo { get; set; }
+        public int CartonNo { get; set; }
+        public string Barcode { get; set; }
+        public string LocationCode { get; set; }
+        public string LastSentDeviceId { get; set; }  
+        public string RequestNo { get; set; }
+    }
+}
---
+++ tlrsCartonManager.DAL/Models/Pick/PickListSearch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tlrsCartonManager.DAL.Models.Pick
+{
+    public class PickListSearch
+    {
+        [Key]
+        public string PickListNo { get; set; }
+        public int CartonNo { get; set; }      
+        public string LocationCode { get; set; }
+        public string LastSentDeviceId { get; set; }
+        public string RequestNo { get; set; }
+    }
+}
