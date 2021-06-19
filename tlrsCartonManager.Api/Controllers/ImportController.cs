a12fe9608179bd5ec78092a737ec09bd6b61c790
Administrator <Administrator@SDDRIT-DEVSRV01>
2021-06-19 09:25:17 +0530

    Import data commit 1

---
+++ tlrsCartonManager.Api/Controllers/ImportController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using tlrsCartonManager.DAL.Dtos;
+using tlrsCartonManager.DAL.Reporsitory.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tlrsCartonManager.DAL.Helper;
+using tlrsCartonManager.Api.Extensions;
+using tlrsCartonManager.DAL.Models.ResponseModels;
+using tlrsCartonManager.DAL.Models;
+using tlrsCartonManager.DAL.Utility;
+using Microsoft.AspNetCore.Authorization;
+using tlrsCartonManager.Api.Error;
+using System.Net;
+using static tlrsCartonManager.DAL.Utility.Status;
+using tlrsCartonManager.DAL.Models.Report;
+using tlrsCartonManager.Services.Report;
+using tlrsCartonManager.Services.Report.Core;
+using tlrsCartonManager.Services.ImportData;
+using Microsoft.AspNetCore.Http;
+using tlrsCartonManager.Core.Enums;
+using tlrsCartonManager.Services.ImportData.Import;
+
+namespace tlrsCartonManager.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Authorize]
+    public class ImportController : Controller
+    {
+        
+        private readonly ImportDataService  _importDataService;
+
+        public ImportController(ImportDataService importDataService)
+        {
+            _importDataService = importDataService;            
+        }       
+
+        [HttpPost("importDataAlternativeNo")]
+        public  IActionResult ImportData(IFormFile file, ImportType importOption)
+        {
+           return Ok( _importDataService.GetImportDetails<ExcelParseAlternativeNoUpdateViewModel>(file, importOption));                 
+        }
+
+    }
+}
