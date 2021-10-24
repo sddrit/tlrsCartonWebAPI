57f4f4bed0056ce245a027d02ef4dade9425f69a
Administrator <Administrator@SDDRIT-DEVSRV01>
2021-10-24 15:25:19 +0530

    name issues

---
+++ tlrsCartonManager.DAL/Models/SystemLogs/Log.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace tlrsCartonManager.DAL.Models
+{
+    [Table("viewLog")]
+    public partial class Log
+    {
+        [Key]
+        public int Id { get; set; }
+        public int LogLevelId { get; set; }
+        [Required]
+        public string ShortMessage { get; set; }
+        public string FullMessage { get; set; }
+        public string CreatedOnUtc { get; set; }
+       
+       
+    }
+}
